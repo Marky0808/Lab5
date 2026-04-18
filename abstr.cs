d20452c9a05a1bbd740232e95026b3326504c6b4
Marky0808 <todikashmangust@gmail.com>
2026-04-18 15:33:21 +0300

    Add files via upload

--- abstr.cs
+++ abstr.cs
@@ -2,5 +2,25 @@ namespace Lab5;
 
 public class abstr
 {
+    public interface ITrackable
+    {
+        string GetTrackingCode();
+    }
     
+    public abstract class Delivery : ITrackable
+    {
+        public string Sender { get; set; }
+        public double Weight { get; set; }
+        public decimal BaseCost { get; set; }
+        
+        protected string TrackingCode { get; set; }
+
+        public Delivery(string sender, double weight, decimal baseCost)
+        {
+            Sender = sender;
+            Weight = weight;
+            BaseCost = baseCost;
+            TrackingCode = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+        }
+    }
 }
